1c2fb33b8fff29698dad434d08766301a60fa7f2
sihang <sihang@0234898f-7f2f-0410-b2a0-ad3834006b92>
2008-09-11 14:53:07 +0000

    move printtet() into meshstat.cxx

--- trunk/src/meshstat.cxx
+++ trunk/src/meshstat.cxx
@@ -5,6 +5,89 @@
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
+// printtet()    Print the detail contents of a tetrahedron.                 //
+//                                                                           //
+///////////////////////////////////////////////////////////////////////////////
+
+void tetgenmesh::printtet(triface* t)
+{
+  triface tmpface, prtface;
+  point tmppt;
+  int facecount;
+
+  printf("Tetra x%lx with loc(%i) and ver(%i):",
+         (unsigned long)(t->tet), t->loc, t->ver);
+  if ((point) t->tet[7] == dummypoint) {
+    printf("  (hull tet)");
+  }
+  if (infected(*t)) {
+    printf(" (infected)");
+  }
+  if (marktested(*t)) {
+    printf(" (marktested)");
+  }
+  printf("\n");
+
+  tmpface = *t;
+  facecount = 0;
+  while(facecount < 4) {
+    tmpface.loc = facecount;
+    sym(tmpface, prtface);
+    if (prtface.tet == NULL) {
+      printf("      [%i] Open !!\n", facecount);
+    } else {
+      printf("      [%i] x%lx  loc(%i).", facecount,
+             (unsigned long)(prtface.tet), prtface.loc);
+      if ((point) prtface.tet[7] == dummypoint) {
+        printf("  (hull tet)");
+      }
+      if (infected(prtface)) {
+        printf(" (infected)");
+      }
+      if (marktested(prtface)) {
+        printf(" (marktested)");
+      }
+      printf("\n");
+    }
+    facecount++;
+  }
+
+  tmppt = org(*t);
+  if(tmppt == (point) NULL) {
+    printf("      Org [%i] NULL\n", locver2org[t->loc][t->ver]);
+  } else {
+    printf("      Org [%i] x%lx (%.12g,%.12g,%.12g) %d\n",
+           locver2org[t->loc][t->ver], (unsigned long)(tmppt),
+           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
+  }
+  tmppt = dest(*t);
+  if(tmppt == (point) NULL) {
+    printf("      Dest[%i] NULL\n", locver2dest[t->loc][t->ver]);
+  } else {
+    printf("      Dest[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
+           locver2dest[t->loc][t->ver], (unsigned long)(tmppt),
+           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
+  }
+  tmppt = apex(*t);
+  if(tmppt == (point) NULL) {
+    printf("      Apex[%i] NULL\n", locver2apex[t->loc][t->ver]);
+  } else {
+    printf("      Apex[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
+           locver2apex[t->loc][t->ver], (unsigned long)(tmppt),
+           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
+  }
+  tmppt = oppo(*t);
+  if(tmppt == (point) NULL) {
+    printf("      Oppo[%i] NULL\n", loc2oppo[t->loc]);
+  } else {
+    printf("      Oppo[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
+           loc2oppo[t->loc], (unsigned long)(tmppt),
+           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
+  }
+}
+
+///////////////////////////////////////////////////////////////////////////////
+//                                                                           //
 // checkmesh()    Test mesh for geometrical and topological consistencies.   //
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
--- trunk/src/tetgen.h
+++ trunk/src/tetgen.h
@@ -976,91 +976,6 @@ void bond(triface& t1, triface& t2) {
 
 ///////////////////////////////////////////////////////////////////////////////
 //                                                                           //
-// Primitives for debugging                                                  //
-//                                                                           //
-///////////////////////////////////////////////////////////////////////////////
-
-void printtet(triface* t)
-{
-  triface tmpface, prtface;
-  point tmppt;
-  int facecount;
-
-  printf("Tetra x%lx with loc(%i) and ver(%i):",
-         (unsigned long)(t->tet), t->loc, t->ver);
-  if ((point) t->tet[7] == dummypoint) {
-    printf("  (hull tet)");
-  }
-  if (infected(*t)) {
-    printf(" (infected)");
-  }
-  if (marktested(*t)) {
-    printf(" (marktested)");
-  }
-  printf("\n");
-
-  tmpface = *t;
-  facecount = 0;
-  while(facecount < 4) {
-    tmpface.loc = facecount;
-    sym(tmpface, prtface);
-    if (prtface.tet == NULL) {
-      printf("      [%i] Open !!\n", facecount);
-    } else {
-      printf("      [%i] x%lx  loc(%i).", facecount,
-             (unsigned long)(prtface.tet), prtface.loc);
-      if ((point) prtface.tet[7] == dummypoint) {
-        printf("  (hull tet)");
-      }
-      if (infected(prtface)) {
-        printf(" (infected)");
-      }
-      if (marktested(prtface)) {
-        printf(" (marktested)");
-      }
-      printf("\n");
-    }
-    facecount++;
-  }
-
-  tmppt = org(*t);
-  if(tmppt == (point) NULL) {
-    printf("      Org [%i] NULL\n", locver2org[t->loc][t->ver]);
-  } else {
-    printf("      Org [%i] x%lx (%.12g,%.12g,%.12g) %d\n",
-           locver2org[t->loc][t->ver], (unsigned long)(tmppt),
-           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
-  }
-  tmppt = dest(*t);
-  if(tmppt == (point) NULL) {
-    printf("      Dest[%i] NULL\n", locver2dest[t->loc][t->ver]);
-  } else {
-    printf("      Dest[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
-           locver2dest[t->loc][t->ver], (unsigned long)(tmppt),
-           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
-  }
-  tmppt = apex(*t);
-  if(tmppt == (point) NULL) {
-    printf("      Apex[%i] NULL\n", locver2apex[t->loc][t->ver]);
-  } else {
-    printf("      Apex[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
-           locver2apex[t->loc][t->ver], (unsigned long)(tmppt),
-           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
-  }
-  tmppt = oppo(*t);
-  if(tmppt == (point) NULL) {
-    printf("      Oppo[%i] NULL\n", loc2oppo[t->loc]);
-  } else {
-    printf("      Oppo[%i] x%lx (%.12g,%.12g,%.12g) %d\n",
-           loc2oppo[t->loc], (unsigned long)(tmppt),
-           tmppt[0], tmppt[1], tmppt[2], pointmark(tmppt));
-  }
-}
-
-void printsh(face* s);
-
-///////////////////////////////////////////////////////////////////////////////
-//                                                                           //
 // The badface structure                                                     //
 //                                                                           //
 // A multiple usages structure. Despite of its name, a 'badface' can be used //
@@ -1413,6 +1328,7 @@ void outvoronoi(tetgenio* out);
 //                                                                           //
 ///////////////////////////////////////////////////////////////////////////////
 
+void printtet(triface* t);
 void checkmesh(memorypool* pool);
 void checkshells();
 void checkdelaunay();
