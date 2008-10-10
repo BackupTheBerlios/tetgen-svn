c90e756a38a81e4e7b8f6e2a447ebd75c4218117
sihang <sihang@0234898f-7f2f-0410-b2a0-ad3834006b92>
2008-10-10 07:51:28 +0000

    restore original version, removed edgellist

--- trunk/src/tetgen.h
+++ trunk/src/tetgen.h
@@ -1311,9 +1311,6 @@ badface *futureflip;
 // Two dynmaic arrays used by Bowyer-Watson algorithm.
 arraypool *cavetetlist, *cavebdrylist;
 
-// A (n + 1) x (n + 1) connection matrix (for Bowyer-Watson algorithm).
-triface *edgellist;
-
 // Variables for accessing data fields (initialized in initializepools()).
 int point2tetindex, pointmarkindex;
 int elemmarkerindex;
@@ -1536,7 +1533,6 @@ void initialize()
   dummypoint = (point) NULL;
   futureflip = (badface *) NULL;
   cavetetlist = cavebdrylist = (arraypool *) NULL;
-  edgellist = (triface *) NULL;
   point2tetindex = pointmarkindex = 0;
   elemmarkerindex = 0;
   elemattribindex = volumeboundindex = highorderindex = 0;
@@ -1584,7 +1580,6 @@ void deinitialize()
   if (cavetetlist != (arraypool *) NULL) {
     delete cavetetlist;
     delete cavebdrylist;
-    delete [] edgellist;
   }
   futureflip = (badface *) NULL;
 }
