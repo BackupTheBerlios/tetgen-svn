aa1043ef73e96e2b759b7c661ecb9aba01bb5e80
sihang <sihang@0234898f-7f2f-0410-b2a0-ad3834006b92>
2009-04-15 12:22:05 +0000

    update constrain.cxx

--- trunk/src/tetgen.h
+++ trunk/src/tetgen.h
@@ -1664,7 +1664,6 @@ enum intersection scoutcrosstet(face* ssub, triface* searchtet, arraypool*);
 void recoversubfacebyflips(face* pssub, triface* crossface, arraypool*);
 void formcavity(face*, arraypool*, arraypool*, arraypool*, arraypool*, 
                 arraypool*, arraypool*);
-void formedgecavity(point, point, arraypool*, arraypool*, arraypool*);
 bool delaunizecavity(arraypool*, arraypool*, arraypool*, arraypool*,
                      arraypool*, arraypool*);
 bool fillcavity(arraypool*, arraypool*, arraypool*, arraypool*);
